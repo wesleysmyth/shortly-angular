5f784f8fa1741ea487570dd30b129965bf6fba7b
wesleysmyth <wesleytatesmith@gmail.com>
2015-01-17 11:49:46 -0800

    Seemingly implements an authorization check for links and shorten

--- client/app/app.js
+++ client/app/app.js
@@ -33,7 +33,7 @@ window.app = angular.module('shortly', [
         controller: 'ShortenController'
       });
 
-    $urlRouterProvider.otherwise('/links');
+    $urlRouterProvider.otherwise('/signin');
 
 
     // We add our $httpInterceptor into the array
@@ -57,7 +57,7 @@ window.app = angular.module('shortly', [
   };
   return attach;
 })
-.run(function ($rootScope, $location, Auth) {
+.run(function ($rootScope, $location, Auth, $state) {
   // here inside the run phase of angular, our services and controllers
   // have just been registered and our app is ready
   // however, we want to make sure the user is authorized
@@ -65,9 +65,15 @@ window.app = angular.module('shortly', [
   // when it does change routes, we then look for the token in localstorage
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
-  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-      $location.path('/signin');
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+    console.log('toState: ', toState.name, 'fromState: ', fromState.name, event)
+    if ((toState.name === 'links' || toState.name === 'shorten') && !Auth.isAuth()) { //(toState.name === 'links' || toState.name === 'shorten') &&
+      console.log('route to signin');
+      event.preventDefault();
+      // console.log($state);
+      // $location.path('/signin');
+      // $state.go('signin');
+      $state.go('signin');
     }
   });
 });
